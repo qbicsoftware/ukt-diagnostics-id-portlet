3615dfe47e415e8e47f29cc588c6ac00dc801ba2
Steffengreiner <Steffen.Greiner@gmx.de>
2022-02-14 11:13:23 +0100

    Update Readme and remove parent-pom

--- src/main/java/life/qbic/MyPortletUI.java
+++ src/main/java/life/qbic/MyPortletUI.java
@@ -3,12 +3,9 @@ package life.qbic;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletSession;
 
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.WrappedPortletSession;
